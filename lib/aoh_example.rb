3672e6a090b1af5858b1af1d0a5c81dc7c36bac9
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-19 17:45:33 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,9 +1,9 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 
-DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
-JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-KATE_G = { name:  "Kate Gompert", occupation: "None" }
+DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
+JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
+KATE_G = { name:  "Kate Gompert", occupation: "None" },
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
